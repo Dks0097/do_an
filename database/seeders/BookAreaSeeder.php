d045ca3571ea863193025f811591223176dcd05f
Dks0097 <101386674+Dks0097@users.noreply.github.com>
2024-01-09 12:29:06 +0700

    book

--- database/seeders/BookAreaSeeder.php
+++ database/seeders/BookAreaSeeder.php
@@ -18,6 +18,8 @@ class BookAreaSeeder extends Seeder
                 'short_title'   => "MAKE A QUICK BOOKING",
                 'main_title'    => "We Are the Best in All-time & So Please Get a Quick Booking",
                 'short_desc'    => "Atoli is one of the best resorts in the global market and that's why you will get a luxury life period on the global market. We always provide you a special support for all of our guests and that's will be the main reason to be the most popular.",
+                'image'    => "upload/bookarea/1787589257875300.png",
+
                 'created_at'    => date('Y-m-d H:i:s', time()),
                 'updated_at'    => date('Y-m-d H:i:s', time()),
             ],
